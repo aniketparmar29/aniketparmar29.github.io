311d2c39e22e23dd946faf1ff94d004c79d4cf46
aniketparmar29 <aniketparmar068@gmail.com>
2022-12-25 15:02:33 +0530

    ASD

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-scroll';
 function Navbar({isDarkMode,toggleDarkMode}) {
    const ophamburger = () =>{
        let hamburger = window.document.querySelector(".hamburger");
-       hamburger.classList="hidden hamburger border p-2 xl:hidden md:hidden sm:hidden mt-8 ";
+       hamburger.classList="hidden hamburger border p-2 xl:hidden md:hidden sm:hidden  ";
        let close = window.document.querySelector(".closeham");
-       close.classList="block space-y-2 m-3 closeham mr-4 border p-2 px-4 mt-8 xl:hidden md:hidden sm:hidden ";
+       close.classList="block space-y-2 m-3 closeham mr-4 border p-2 px-4  xl:hidden md:hidden sm:hidden ";
        let menui = window.document.querySelector(".menuiji");
        menui.classList=isDarkMode===true?" flex-col  menuiji px-10 py-10 space-x-11 justify-end text-[45px]   sm:hidden xl:hidden   md:hidden ":"bg-slate-300 block flex-col  menuiji px-10 py-10 space-x-11 justify-end text-[45px]   sm:hidden xl:hidden   md:hidden ";
        let navbar = window.document.querySelector(".navbarj");
@@ -15,9 +15,9 @@ function Navbar({isDarkMode,toggleDarkMode}) {
    }
     const closehamburger = () =>{
        let jam = window.document.querySelector(".closeham");
-       jam.classList="hidden closeham xl:hidden md:hidden sm:hidden mt-8";
+       jam.classList="hidden closeham xl:hidden md:hidden sm:hidden ";
        let hamburger = window.document.querySelector(".hamburger");
-       hamburger.classList="block space-y-2 m-3 hamburger border p-2 mt-8 xl:hidden md-hidden sm:hidden";
+       hamburger.classList="block space-y-2 m-3 hamburger border p-2 xl:hidden md-hidden sm:hidden";
        let menui = window.document.querySelector(".menuiji");
        menui.classList=isDarkMode===true?" hidden   menuiji px-28 py-4 space-x-11 justify-end text-[45px] sm-hidden xl:hidden  md:hidden ":"bg-slate-300 hidden   menuiji px-28 py-4 space-x-11 justify-end text-[45px] sm-hidden xl:hidden  md:hidden ";
        let navbar = window.document.querySelector(".navbarj");
@@ -27,13 +27,13 @@ function Navbar({isDarkMode,toggleDarkMode}) {
         
     },[isDarkMode])
   return (
-    <div name="Navbar" className='lg:sticky md:sticky xl:sticky top-0 z-100'>
-        <nav className=' navbarj flex p-0 justify-between  border-b-2 border-slate-900 ' style={isDarkMode===true?{backgroundColor:"rgb(8, 16, 28)",color:"white"}:{backgroundColor:"rgb(144, 175, 224)",color:"black"}} >
+    <div name="Navbar" className='lg:sticky md:sticky xl:sticky sticky  top-0 z-100'>
+        <nav className=' navbarj flex  justify-between  border-b-2  border-slate-900 ' style={isDarkMode===true?{backgroundColor:"rgb(8, 16, 28)",color:"white"}:{backgroundColor:"rgb(144, 175, 224)",color:"black"}} >
             <div className='flex xl:w-[130px] w-[100%] justify-between'>
-            <Link to="Home" smooth duration={1500}><img  src={isDarkMode===true?"https://pbs.twimg.com/media/FkbPnfGWYAEEgJI?format=png&name=360x360":"https://pbs.twimg.com/media/FkbPW5IX0AgIVlj?format=png&name=360x360"} alt="logo" /></Link>
+            <Link to="Home" smooth duration={1500}><img className='xl:h-[100%] h-[50%]' src={isDarkMode===true?"https://pbs.twimg.com/media/FkbPnfGWYAEEgJI?format=png&name=360x360":"https://pbs.twimg.com/media/FkbPW5IX0AgIVlj?format=png&name=360x360"} alt="logo" /></Link>
             <div>
             <h1 onClick={closehamburger} style={isDarkMode===true?{border:"1px solid white"}:{border:"1px solid black"}} className='space-y-2 m-3 xl:hidden md:hidden sm:hidden  border p-2 xl:text-[100px] md:text-[70px] hidden closeham'>X</h1>
-            <div onClick={ophamburger} className="hamburger border p-2 mt-8 xl:hidden md:hidden sm:hidden space-y-2 m-3" style={isDarkMode===true?{border:"1px solid white"}:{border:"1px solid black"}}>
+            <div onClick={ophamburger} className="hamburger border p-2  xl:hidden md:hidden sm:hidden space-y-2 m-3" style={isDarkMode===true?{border:"1px solid white"}:{border:"1px solid black"}}>
                 <div className="w-8 h-0.5 bg-gray-600"></div>
                 <div className="w-8 h-0.5 bg-gray-600"></div>
                 <div className="w-8 h-0.5 bg-gray-600"></div>
